7cbb521836528e3c2fd8df3588ed0102ab302644
LAPTOP-UHC40UK6\chand <chosteler@mail.pima.edu>
2017-10-18 00:15:00 -0700

    Added left right and up down platform codes, ill improve them bit later

---
+++ New Unity Project/Assets/Platforms/LeftRightPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRightPlatform : MonoBehaviour
+{
+
+    private Vector2 startPosition;
+    private int speed;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        speed = 3;
+    }
+
+    void Update()
+    {
+        transform.position =
+            new Vector2(startPosition.x + Mathf.Sin(Time.time * speed), transform.position.y);
+    }
+}
+    
