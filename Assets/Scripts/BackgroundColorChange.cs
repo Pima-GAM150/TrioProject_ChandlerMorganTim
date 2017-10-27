9f630ad4f99fe8f943c9118aa14df617d1add7e8
Morgan Ferguson <mfergu>
2017-10-27 16:39:00 -0700

    First level background colors added.

---
+++ Assets/Scripts/BackgroundColorChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundColorChange : MonoBehaviour {
+
+	public Sprite bluedim;
+	public Sprite reddim;
+	public Sprite greendim;
+	public Sprite whitedim;
+
+	public SpriteRenderer backgroundplatform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (PlayerController.playerModeSelect == 1) {
+			backgroundplatform.sprite = bluedim;
+		}
+		if (PlayerController.playerModeSelect == 2) {
+			backgroundplatform.sprite = reddim;
+		}
+		if (PlayerController.playerModeSelect == 3) {
+			backgroundplatform.sprite = greendim;
+		}
+		if (PlayerController.playerModeSelect == 0) {
+			backgroundplatform.sprite = whitedim;
+		}
+
+	}
+}
